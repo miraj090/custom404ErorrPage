180ce2e6acc784b46369f3145c7194937f46757e
miraj090 <ambroseffc29@gmail.com>
2024-08-11 21:11:35 +0600

    3rd

---
+++ error.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('triggerButton').addEventListener('click', function() {
+        $('#errorModal').modal('show');
+    });
+});
